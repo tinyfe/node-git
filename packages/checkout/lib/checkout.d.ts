0fe0d0ec20df8809c6dee5bf422887deab703c89
Rain120 <chao1202016@outlook.com>
2021-08-08 16:15:08 +0800

    feat: remove checkout lib

--- packages/checkout/lib/checkout.d.ts
+++
@@ -1,9 +0,0 @@
-import { fn } from './typings';
-export declare function checkout(checkout: string, { targetPath, git }?: {
-    targetPath?: string | undefined;
-    git?: string | undefined;
-}, cb?: fn): void;
-export declare const checkoutPromisify: (arg1: string, arg2: {
-    targetPath?: string | undefined;
-    git?: string | undefined;
-} | undefined) => Promise<any>;
--- packages/checkout/lib/processAsync.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { ChildProcess } from 'child_process';
-import { fn } from './typings';
-export declare function processAsync(process: ChildProcess, cmd: string, cb?: fn): void;
